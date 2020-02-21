8836be7697a70ab168b10d015b4ed91e5cf2849b
sasa0421 <46702992+sasa0421@users.noreply.github.com>
2020-02-21 15:16:09 -0800

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,3 @@
-
-
 var data = require("../users.json");
 
 //function _isContains(json, value) {
@@ -15,20 +13,23 @@ exports.auth = function(req, res) {
 var login = {"name": req.query.name,
 "description": req.query.description};
 console.log(login);
-for (i=0; i < data.users.length; i++) {
-console.log(data.users[i]);
-if (data.users[i].name == login.name && data.users[i].password == login.description) {
-data.users[i].isUser = true;
-var user = data.users[i];
 res.render('sip', user);
-break;
 }
-else if (data.users[i].name == "Test") {
-data.users[i].isUser = true;
-var user = data.users[i];
-res.render('sip', user);
-break;
-}
-}
-res.render('index');
-}
+// for (i=0; i < data.users.length; i++) {
+// 	console.log(data.users[i]);
+// 	if (data.users[i].name == login.name && data.users[i].password == login.description) {
+// 		data.users[i].isUser = true;
+// 		var user = data.users[i];
+// 		res.render('sip', user);
+// 		break;
+// 	}
+// 	else if (data.users[i].name == "Test") {
+// 		data.users[i].isUser = true;
+// 		var user = data.users[i];
+// 		res.render('sip', user);
+// 		break;
+// 	}
+// }
+// 	res.render('index');
+// }
+
