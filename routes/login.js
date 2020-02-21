482e28a746af4ff1d3a447517ada8d16f74e8b12
sasa0421 <46702992+sasa0421@users.noreply.github.com>
2020-02-21 15:28:36 -0800

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -1,20 +1,32 @@
-var data = require("../accounts.json");
+var data = require("../users.json");
 
-function _isContains(json, value) {
-    let contains = false;
-    Object.keys(json).some(key => {
-        contains = typeof json[key] === 'object' ? _isContains(json[key], value) : json[key] === value;
-         return contains;
-    });
-    return contains;
- }
+//function _isContains(json, value) {
+//    let contains = false;
+//    Object.keys(json).some(key => {
+//        contains = typeof json[key] === 'object' ? _isContains(json[key], value) : json[key] === value;
+//         return contains;
+//    });
+//    return contains;
+// }
 
 exports.auth = function(req, res) {
-	var namez = {"name": req.query.name,
-				"description": req.query.description};
-	if (_isContains(data, req.query.name) && _isContains(data, req.query.description)) {
-		res.render('sip');
-	} else {
-		res.render('index');
-	}
+var login = {"name": req.query.name,
+"description": req.query.description};
+console.log(login);
+for (i=0; i < data.users.length; i++) {
+console.log(data.users[i]);
+if (data.users[i].name == login.name && data.users[i].password == login.description) {
+data.users[i].isUser = true;
+var user = data.users[i];
+res.render('sip', user);
+break;
+}
+else if (data.users[i].name == "Test") {
+data.users[i].isUser = true;
+var user = data.users[i];
+res.render('sip', user);
+break;
+}
+}
+res.render('index');
 }
